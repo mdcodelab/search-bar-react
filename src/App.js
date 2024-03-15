f49701b91309c605818c152dac86f25ba3ae9e20
miha11-hash <d_mihaela@msn.com>
2024-03-15 19:41:18 +0200

    search functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
 import { FaSearch } from "react-icons/fa";
-import "./App.css"
-import BookData from "./Data.json";
+import "./App.css";
+import data from "./Data.json";
 import { IoMdClose } from "react-icons/io";
 
 function App() {
+  const [filteredData, setFilteredData] = React.useState([]);
+
+  function handleFilter(e) {
+    const searchWord=e.target.value;
+    const newFiltered=data.filter((value) => value.title.toLowerCase().includes(searchWord.toLowerCase()))
+    setFilteredData(newFiltered);
+    if(searchWord === "") {
+      setFilteredData([]);
+    }
+
+  }
+
   return (
     <div className="app">
       <div className="search">
         <h2>Search for books</h2>
         <div className="searchInputs">
-          <input type="text" placeholder="Search for books"></input>
+          <input type="text" placeholder="Enter a book name" onChange={handleFilter}/>
           <FaSearch
             className="icon"
             style={{
@@ -20,9 +32,20 @@ function App() {
               fontSize: "2rem",
               cursor: "pointer",
             }}
-          ></FaSearch>
+          />
         </div>
       </div>
+      {filteredData.length > 0 ? (
+        <div className="results">
+          {filteredData.slice(0,15).map((book, index) => {
+            return (
+              <a key={index} href={book.link} target="_blank">
+                {book.title}
+              </a>
+            );
+          })}
+        </div>
+      ) : null}
     </div>
   );
 }
