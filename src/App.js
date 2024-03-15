c84eb4e1da31136cc55414d1a2e05d8d09af5249
miha11-hash <d_mihaela@msn.com>
2024-03-15 17:24:41 +0200

    JSX & style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { FaSearch } from "react-icons/fa";
+import "./App.css"
+import BookData from "./Data.json";
+import { IoMdClose } from "react-icons/io";
 
 function App() {
   return (
-    <div>
-      hello
+    <div className="app">
+      <div className="search">
+        <h2>Search for books</h2>
+        <div className="searchInputs">
+          <input type="text" placeholder="Search for books"></input>
+          <FaSearch
+            className="icon"
+            style={{
+              position: "absolute",
+              top: "15px",
+              right: "0.5rem",
+              fontSize: "2rem",
+              cursor: "pointer",
+            }}
+          ></FaSearch>
+        </div>
+      </div>
     </div>
   );
 }
